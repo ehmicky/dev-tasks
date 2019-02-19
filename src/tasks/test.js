812d7222d1bf167365676a27ddc9c93dba1ee19b
ehmicky <ehmicky@gmail.com>
2019-02-19 10:00:00 +0100

    Build before testing

--- src/tasks/test.js
+++ src/tasks/test.js
@@ -5,9 +5,10 @@ const { series } = require('gulp')
 const { getWatchTask } = require('../utils')
 
 const { check } = require('./check')
+const { build } = require('./build')
 const { unit } = require('./unit')
 
-const testTask = series(check, unit)
+const testTask = series(check, build, unit)
 
 // eslint-disable-next-line fp/no-mutation
 testTask.description = 'Lint and test source files'
