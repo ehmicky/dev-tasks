38b3da2de7a4e754b7df0c13bb0b5771a8799b66
ehmicky <ehmicky@gmail.com>
2019-02-19 10:00:00 +0100

    Do not use ava.config.js

--- src/tasks/unit.js
+++ src/tasks/unit.js
@@ -1,22 +1,35 @@
 'use strict'
 
+const { platform } = require('process')
+
 const { getWatchTask } = require('../utils')
 const gulpExeca = require('../exec')
 
 const { hasCoverage, uploadCoverage, checkCoverage } = require('./coverage')
 
 const unit = async function() {
+  const flags = getAvaFlags()
+
   const shouldCoverage = await hasCoverage()
 
   if (!shouldCoverage) {
-    return gulpExeca('ava')
+    return gulpExeca(`ava ${flags}`)
   }
 
   await gulpExeca(
-    'nyc --reporter=lcov --reporter=text --reporter=html --reporter=json ava',
+    `nyc --reporter=lcov --reporter=text --reporter=html --reporter=json ava ${flags}`,
   )
 
   await uploadCoverage()
+}
+
+// Workaround for https://github.com/istanbuljs/istanbuljs/issues/141
+const getAvaFlags = function() {
+  if (platform !== 'win32') {
+    return ''
+  }
+
+  return '--serial'
 }
 
 // eslint-disable-next-line fp/no-mutation
