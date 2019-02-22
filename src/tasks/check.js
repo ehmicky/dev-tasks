dd65e4a31c8b7c277cb2fd06646acd4b349d06db
ehmicky <ehmicky@gmail.com>
2019-02-22 10:00:00 +0100

    Improve gulp-eslint

--- src/tasks/check.js
+++ src/tasks/check.js
@@ -1,6 +1,8 @@
 'use strict'
 
-const { series, parallel } = require('gulp')
+const { src, dest, series, parallel, lastRun } = require('gulp')
+const gulpEslint = require('gulp-eslint')
+const gulpIf = require('gulp-if')
 
 const { CHECK } = require('../files')
 const gulpExeca = require('../exec')
@@ -19,12 +21,35 @@ const eslint = function() {
   )
 }
 
+const eslintWatch = function() {
+  return src(CHECK, { dot: true, since: lastRun(eslintWatch) })
+    .pipe(
+      gulpEslint({
+        ignorePath: '.gitignore',
+        fix: true,
+        maxWarnings: 0,
+        // eslint-disable-next-line id-length
+        reportUnusedDisableDirectives: true,
+      }),
+    )
+    .pipe(gulpEslint.format('codeframe'))
+    .pipe(gulpEslint.failAfterError())
+    .pipe(gulpIf(isFixed, dest(getFileBase)))
+}
+
+const isFixed = function({ eslint: { fixed } = {} }) {
+  return fixed
+}
+
+const getFileBase = function({ base }) {
+  return base
+}
+
 const escapePattern = function(pattern) {
   return `"${pattern}"`
 }
 
 const lint = series(format, eslint)
-
 const check = parallel(lint, jscpd)
 
 // eslint-disable-next-line fp/no-mutation
@@ -35,4 +60,6 @@ const checkw = getWatchTask(check, CHECK)
 module.exports = {
   check,
   checkw,
+  eslint,
+  eslintWatch,
 }
