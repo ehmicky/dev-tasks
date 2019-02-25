971b93d4f29b8aceec574b3c873de97e165afccf
ehmicky <ehmicky@gmail.com>
2019-02-25 10:00:00 +0100

    Improve watch mode

--- src/watch.js
+++ src/watch.js
@@ -1,6 +1,13 @@
 'use strict'
 
+const { promisify } = require('util')
+const {
+  argv: [, script, ...args],
+  env: { GULP_WATCH },
+} = require('process')
+
 const { watch, parallel } = require('gulp')
+const Nodemon = require('nodemon')
 
 // Watch files to run a task.
 // Returns the watch task.
@@ -9,12 +16,51 @@ const getWatchTask = function(
   task,
   { initial = true, ...watchOpts } = {},
 ) {
-  // We do not use `func.bind()` to make the task name `watchTask` instead
-  // of `bound watchTask`
+  const watchTask = getTask({ files, task, initial, watchOpts })
+  addDescription({ watchTask, task, initial })
+  return watchTask
+}
+
+// We do not use `func.bind()` to make the task name `watchTask` instead
+// of `bound watchTask`
+const getTask = function({ files, task, initial, watchOpts }) {
+  if (GULP_WATCH !== 'no-restart') {
+    return () => startNodemon()
+  }
+
   const watchTask = () => watch(files, task, watchOpts)
   const watchTaskA = addInitial({ watchTask, task, initial })
-  addDescription({ watchTask: watchTaskA, task, initial })
   return watchTaskA
+}
+
+// We relaunch the same CLI command `gulp ...` but inside Nodemon.
+// I.e. watching relaunches gulp when either:
+//  - Gulp tasks changed
+//  - Modules have been installed, updated or uninstalled
+// This works with `gulp` (default task) and `gulp taskA taskB` (multiple tasks)
+// We use an environment variable `GULP_WATCH` to distinguish between these
+// modes
+const startNodemon = async function() {
+  const nodemon = new Nodemon({ ...NODEMON_CONFIG, script, args })
+
+  // Otherwise Nodemon logs are silent
+  // eslint-disable-next-line no-console, no-restricted-globals
+  nodemon.on('log', ({ colour }) => console.log(colour))
+
+  await promisify(nodemon.on.bind(nodemon))('start')
+}
+
+const NODEMON_CONFIG = {
+  env: { GULP_WATCH: 'no-restart' },
+  watch: [
+    'gulpfile.*',
+    'gulpfile.*.*',
+    'gulp/',
+    'package.json',
+    'package-lock.json',
+    'yarn.lock',
+  ],
+  delay: 100,
 }
 
 const addInitial = function({ watchTask, task, initial }) {
