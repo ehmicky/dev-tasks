0996b53472f2c5754fa3c136b0f244744a4af84b
ehmicky <ehmicky@gmail.com>
2019-03-01 10:00:00 +0100

    Improve files.json

--- src/tasks/build.js
+++ src/tasks/build.js
@@ -6,29 +6,27 @@ const del = require('del')
 const yamlToJson = require('gulp-yaml')
 const mapSources = require('@gulp-sourcemaps/map-sources')
 
-const { SRC, DIST } = require('../files')
+const { SRC, TEST, DIST } = require('../files')
 const { getWatchTask } = require('../watch')
+
+const BUILD = `{${SRC},${TEST}}/**`
 
 const clean = () => del(DIST)
 
 const copy = () =>
-  src(
-    ['{src,test}/**/*[^~]', '!{src,test}/**/*.js', '!{src,test}/**/*.y{,a}ml'],
-    { dot: true, since: lastRun(copy) },
-  ).pipe(dest(DIST))
+  src([`${BUILD}/*[^~]`, `!${BUILD}/*.js`, `!${BUILD}/*.y{,a}ml`], {
+    dot: true,
+    since: lastRun(copy),
+  }).pipe(dest(DIST))
 
 const babel = () =>
-  src('{src,test}/**/*.js', {
-    dot: true,
-    since: lastRun(babel),
-    sourcemaps: true,
-  })
+  src(`${BUILD}/*.js`, { dot: true, since: lastRun(babel), sourcemaps: true })
     .pipe(gulpBabel({ comments: false, minified: true, retainLines: true }))
     .pipe(mapSources(path => `../../${path}`))
     .pipe(dest(DIST, { sourcemaps: '.' }))
 
 const yaml = () =>
-  src('{src,test}/**/*.y{,a}ml', { dot: true, since: lastRun(yaml) })
+  src(`${BUILD}/*.y{,a}ml`, { dot: true, since: lastRun(yaml) })
     .pipe(yamlToJson({ schema: 'JSON_SCHEMA', space: 2 }))
     .pipe(dest(DIST))
 
@@ -38,7 +36,7 @@ const build = series(clean, rebuild)
 // eslint-disable-next-line fp/no-mutation
 build.description = 'Build source files'
 
-const buildw = getWatchTask(SRC, rebuild, { initial: build })
+const buildw = getWatchTask([SRC, TEST], rebuild, { initial: build })
 
 module.exports = {
   build,
