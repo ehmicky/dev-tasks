c9a5f36bca15ddbd0184a4867e5f7c56788a26b0
ehmicky <ehmicky@gmail.com>
2019-03-11 10:00:00 +0100

    Add bindContext()

--- src/utils.js
+++ src/utils.js
@@ -4,13 +4,21 @@ const keepFuncProps = require('keep-func-props')
 const asyncDone = require('async-done')
 
 // Like Function.bind() but:
-//  - does not bind context
+//  - does not need to specify context
 //  - keeps function properties (`name`, etc.)
+// We use Function.bind() to avoid creating extra stack trace frames.
+// However this means context is always bound to `null`
 const bindFunc = function(func, ...args) {
   return func.bind(null, ...args)
 }
 
 const bind = keepFuncProps(bindFunc)
+
+const bindContextFunc = function(func, context) {
+  return func.bind(context)
+}
+
+const bindContext = keepFuncProps(bindContextFunc)
 
 // Ignore rejection of an async function
 const silentAsync = function(func) {
@@ -33,6 +41,7 @@ const asyncDonePromise = function(func) {
 
 module.exports = {
   bind,
+  bindContext,
   silentAsync: eSilentAsync,
   asyncDonePromise,
 }
