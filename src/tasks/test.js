d72afffda3dade92a98e75b0910ad039e96eaa0e
ehmicky <ehmicky@gmail.com>
2019-03-11 10:00:00 +0100

    Fix gulp test order

--- src/tasks/test.js
+++ src/tasks/test.js
@@ -6,7 +6,7 @@ const { check } = require('./check')
 const { build } = require('./build')
 const { unit } = require('./unit')
 
-const testTask = series(build, check, unit)
+const testTask = series(check, build, unit)
 
 // eslint-disable-next-line fp/no-mutation
 testTask.description = 'Build, lint and test source files'
