f834d8ec26574c4c34d6aea3c9810c6c20bbb57d
ehmicky <ehmicky@gmail.com>
2019-03-21 10:00:00 +0100

    Update to latest Babel

--- src/tasks/build/.babelrc.js
+++ src/tasks/build/.babelrc.js
@@ -4,10 +4,9 @@ module.exports = {
   presets: [
     [
       '@babel/preset-env',
-      { targets: { node: '8.10.0' }, useBuiltIns: 'usage' },
+      { targets: { node: '8.10.0' }, useBuiltIns: 'usage', corejs: '3' },
     ],
   ],
-  plugins: ['@babel/plugin-transform-runtime'],
   comments: false,
   shouldPrintComment: comment => comment.includes('istanbul ignore'),
   minified: true,
