83279bfc3ddec160c7bc1615445b0a77e76cc6a1
ehmicky <ehmicky@gmail.com>
2019-03-27 10:00:00 +0100

    Add workaround for lolex

--- src/tasks/unit/log_process_errors.js
+++ src/tasks/unit/log_process_errors.js
@@ -10,6 +10,16 @@ const isSelf = function() {
   return basename(cwd()) === 'log-process-errors'
 }
 
+// TODO: remove once https://github.com/sinonjs/lolex/issues/232 is solved
+const warning = function({ value }) {
+  if (
+    value instanceof Error &&
+    value.message.includes('queueMicrotask() is experimental')
+  ) {
+    return 'silent'
+  }
+}
+
 if (!isSelf()) {
-  logProcessErrors({ testing: 'ava' })
+  logProcessErrors({ testing: 'ava', level: { warning } })
 }
