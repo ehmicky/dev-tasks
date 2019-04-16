ee8bddc485175dfb1f826b7c06a8e40d91acf1cf
ehmicky <ehmicky@gmail.com>
2019-04-16 10:00:00 +0200

    Fix gulp test name

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-export { testTask } from './tasks/test.js'
+export { testTask as test } from './tasks/test.js'
 export { build, buildw } from './tasks/build/main.js'
 export { check, checkw } from './tasks/check/main.js'
 export { unit, unitw, coverage } from './tasks/unit/main.js'
