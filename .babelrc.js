ade68d28ed6346422a34b852fb8034a52d8db13f
ehmicky <ehmicky@gmail.com>
2019-04-17 10:00:00 +0200

    Add comment

--- .babelrc.js
+++ .babelrc.js
@@ -1,7 +1,8 @@
 'use strict'
 
 // Meant to be used by caller only, as `presets`.
-// Only useful when caller needs to use `@babel/register`, e.g. when using
+// Only useful when caller needs to do some Babel compilation outside of
+// `gulp build`. This happens for example when using `@babel/register` through
 // `gulpfile.babel.js`.
 // This cannot be a JSON file because that does not work with `presets`.
 module.exports = require('./build/src/tasks/build/.babelrc.js')
