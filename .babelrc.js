e30176f31898ffc5b7544ae10ef391cb36dafc98
ehmicky <ehmicky@gmail.com>
2019-04-17 10:00:00 +0200

    Add comment

--- .babelrc.js
+++ .babelrc.js
@@ -1,6 +1,6 @@
 'use strict'
 
-// Meant to be used by caller as `presets`.
+// Meant to be used by caller only, as `presets`.
 // Only useful when caller needs to use `@babel/register`, e.g. when using
 // `gulpfile.babel.js`.
 // This cannot be a JSON file because that does not work with `presets`.
