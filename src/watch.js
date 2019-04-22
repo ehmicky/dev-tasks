f5eb64ef24a64dda9e8833645b08141d4dd86a06
ehmicky <ehmicky@gmail.com>
2019-04-22 10:00:00 +0200

    Improve gulp-watch

--- src/watch.js
+++ src/watch.js
@@ -2,16 +2,39 @@ import { promisify } from 'util'
 
 import { watch } from 'gulp'
 
-// Watch files to run a task.
-// Returns the watch task.
-export const getWatchTask = function(files, task, watchOptions) {
-  const watchOptionsA = { ...DEFAULT_WATCH_OPTIONS, ...watchOptions }
+// Same as `gulp.watch()` except:
+//  - return directly a Gulp task.
+//  - default `ignoreInitial` to `false`, i.e. first run the task before
+//    watching.
+//  - wait for watching to be initialized before resolving the task.
+//  - automatically add a task description.
+export const getWatchTask = function(files, firstArg, secondArg) {
+  const { watchOptions, task } = parseOptions(firstArg, secondArg)
   // We do not use `func.bind()` to make the task name `watchTask` instead
   // of `bound watchTask`
-  const watchTask = () =>
-    startWatch({ files, task, watchOptions: watchOptionsA })
+  const watchTask = () => startWatch({ files, task, watchOptions })
   addDescription({ watchTask, task })
   return watchTask
+}
+
+// `gulp.watch()` allow both `watchOptions` and `task` to be optional.
+// We need to normalize this before adding default options.
+const parseOptions = function(firstArg, secondArg) {
+  const { watchOptions, task } = parseOptionals(firstArg, secondArg)
+  const watchOptionsA = { ...DEFAULT_WATCH_OPTIONS, ...watchOptions }
+  return { watchOptions: watchOptionsA, task }
+}
+
+const parseOptionals = function(firstArg, secondArg) {
+  if (typeof firstArg === 'function' || !isObject(firstArg)) {
+    return { task: firstArg }
+  }
+
+  return { watchOptions: firstArg, task: secondArg }
+}
+
+const isObject = function(value) {
+  return value !== null && typeof value === 'object'
 }
 
 const DEFAULT_WATCH_OPTIONS = { ignoreInitial: false }
@@ -22,10 +45,8 @@ const startWatch = async function({ files, task, watchOptions }) {
   }
 
   const watcher = watch(files, watchOptions, task)
-  await Promise.race([
-    promisify(watcher.on.bind(watcher))('ready'),
-    promisify(watcher.on.bind(watcher))('error'),
-  ])
+  const watcherOn = promisify(watcher.on.bind(watcher))
+  await Promise.race([watcherOn('ready'), watcherOn('error')])
 }
 
 // Add Gulp `taks.description` by re-using the watched task's description
