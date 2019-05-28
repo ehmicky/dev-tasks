2fcc815c2d3c58e6d43e8b9529f6c63c3f517b00
ehmicky <ehmicky@gmail.com>
2019-05-28 10:00:00 +0200

    Remove dead code

--- src/utils.js
+++ src/utils.js
@@ -12,13 +12,6 @@ const bindFunc = function(func, ...args) {
 
 export const bind = keepFuncProps(bindFunc)
 
-// To use once we abstract `bindFunc()`
-// const bindContextFunc = function(func, context) {
-//   return func.bind(context)
-// }
-
-// export const bindContext = keepFuncProps(bindContextFunc)
-
 // Ignore rejection of an async function
 const eSilentAsync = function(func) {
   return async (...args) => {
