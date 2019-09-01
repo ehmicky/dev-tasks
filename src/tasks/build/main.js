345790583198597bf5d1e66bff1fac5cb2c53701
ehmicky <ehmicky@gmail.com>
2019-09-01 15:03:46 +0200

    Add comment

--- src/tasks/build/main.js
+++ src/tasks/build/main.js
@@ -15,6 +15,8 @@ const SOURCES_GLOB = `{${BUILD_SOURCES.join(',')}}/**`
 // We remove files deeply but leave empty [sub]directories. Otherwise it creates
 // issues with `chokidar` (file waching used by `ava --watch` and
 // `gulp.watch()`)
+// TODO: replace with `promisify(fs.rmdir)(..., {recursive: true})` after
+// dropping support for Node <12
 const clean = () => del(`${BUILD}/**`, { onlyFiles: true })
 
 const copy = () =>
