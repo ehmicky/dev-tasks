b5ec7c5a8724dd6aa96e628768ebf0db2a988393
ehmicky <ehmicky@gmail.com>
2019-10-13 13:00:03 +0200

    Make core-js optional

--- src/tasks/build/.babelrc.js
+++ src/tasks/build/.babelrc.js
@@ -1,13 +1,19 @@
 'use strict'
 
+const { cwd } = require('process')
+const { dependencies } = require(`${cwd()}/package.json`)
+
+// Using `core-js` as a dependency is optional
+const presetEnvOptions =
+  dependencies['core-js'] === undefined
+    ? {}
+    : { useBuiltIns: 'usage', corejs: '3' }
+
 // This file is loaded before `gulp build` by `@babel/register`, i.e. before any
 // Babel compilation, so it cannot use ES modules.
 module.exports = {
   presets: [
-    [
-      '@babel/preset-env',
-      { targets: { node: '8.10.0' }, useBuiltIns: 'usage', corejs: '3' },
-    ],
+    ['@babel/preset-env', { targets: { node: '8.10.0' }, ...presetEnvOptions }],
   ],
   comments: false,
   shouldPrintComment: comment => comment.includes('istanbul ignore'),
