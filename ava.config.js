02a9f16ac9dacc87815cefae859003455d016538
ehmicky <ehmicky@gmail.com>
2020-01-12 15:52:12 +0100

    Update Ava configuration file for Ava 3

--- ava.config.js
+++ ava.config.js
@@ -13,16 +13,9 @@ export default {
   // We watch only for `*.js` files, otherwise `*.js.map` gets watched and it
   // creates issues.
   files: [`${BUILD_TEST}/**/*.js`],
-  helpers: [`${BUILD_TEST}/helpers/**/*.js`],
-  // Otherwise tests are triggered in watch mode on `src` changes too,
-  // i.e. triggered twice.
-  sources: [`${BUILD}/**/*.js`],
+  ignoredByWatcher: [`!${BUILD}/**/*.js`],
   verbose: true,
-  // We have already compiled the tests with Babel.
-  // Letting ava compile creates too many issues.
-  babel: false,
-  // Must be used otherwise babel is still performed
-  compileEnhancements: false,
   // Use `log-process-errors`
   require: [LOG_PROCESS_ERRORS],
+  timeout: 0,
 }
