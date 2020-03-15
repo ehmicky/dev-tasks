9b7176f033617f80f0ecc6afff4daad0d8f8924c
ehmicky <ehmicky@gmail.com>
2020-03-15 21:43:53 +0100

    Switch to GitHub actions

--- src/tasks/release/publish.js
+++ src/tasks/release/publish.js
@@ -1,19 +1,17 @@
 import { promises } from 'fs'
 
-import { series } from 'gulp'
 import isCi from 'is-ci'
 import PluginError from 'plugin-error'
 import { exec } from 'gulp-execa'
 
 import { NPMRC } from '../../files.js'
-import { build } from '../build/main.js'
 
 // The release process has two steps:
 //   - `gulp release` is performed locally. It tags the commit and creates a
 //     GitHub release.
 //   - `gulp publish` is performed on CI if `gulp release` was used, and after
 //     all tests have passed. It publishes to npm.
-const npmPublish = async function () {
+export const publish = async function () {
   if (!isCi) {
     throw new PluginError(
       'gulp-publish',
@@ -30,9 +28,6 @@ const npmPublish = async function () {
 // does not get leaked in CI logs.
 // eslint-disable-next-line no-template-curly-in-string
 const NPMRC_CONTENT = '//registry.npmjs.org/:_authToken=${NPM_TOKEN}\n'
-
-// We do not need to run tests since they were performed in previous stages.
-export const publish = series(build, npmPublish)
 
 // eslint-disable-next-line fp/no-mutation
 publish.description = 'Publish to npm'
