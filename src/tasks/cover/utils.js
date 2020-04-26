7408ddd9d4c4959323f1f35b581aadfbb706dc55
ehmicky <ehmicky@gmail.com>
2020-04-26 13:58:56 +0200

    Re-enable test coverage

--- src/tasks/cover/utils.js
+++ src/tasks/cover/utils.js
@@ -1,17 +1,13 @@
-// import { env } from 'process'
+import { env } from 'process'
 
-// import isCi from 'is-ci'
+import isCi from 'is-ci'
 
-// const { COVERAGE } = env
+const { COVERAGE } = env
 
 // Only run test coverage on CI because it's slow.
 // Test coverage can also be opted out with the `COVERAGE=false` environment
 // variable for example when there is no source code
 // (e.g. `@ehmicky/eslint-config`).
 export const shouldCover = function () {
-  // There is an ongoing with Codecov which prevents uploading test coverage
-  //   https://github.com/codecov/codecov-action/issues/68
-  // TODO: once fixed, remove next line and uncomment the line after that
-  return false
-  // return isCi && COVERAGE !== 'false'
+  return isCi && COVERAGE !== 'false'
 }
