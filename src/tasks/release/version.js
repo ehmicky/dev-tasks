52c54f0a5330aa5126033d229f03d04bf52194a2
ehmicky <ehmicky@gmail.com>
2020-09-06 17:10:02 +0200

    Remove `node-latest`

--- src/tasks/release/version.js
+++ src/tasks/release/version.js
@@ -1,13 +1,16 @@
 import { version } from 'process'
 
-import { npm } from 'node-latest'
+import execa from 'execa'
+import got from 'got'
 import nodeVersionAlias from 'node-version-alias'
 import PluginError from 'plugin-error'
 
 // Make sure the latest Node and npm versions are used when releasing
 const checkVersion = async function (name, { current, latest }) {
-  const currentVersion = current()
-  const latestVersion = await latest()
+  const [currentVersion, latestVersion] = await Promise.all([
+    current(),
+    latest(),
+  ])
 
   if (latestVersion === currentVersion) {
     return
@@ -32,7 +35,25 @@ const checkNodeVersion = checkVersion.bind(undefined, 'Node.js', {
   current: getCurrentNode,
   latest: getLatestNode,
 })
-const checkNpmVersion = checkVersion.bind(undefined, 'npm', npm)
+
+const getCurrentNpm = async function () {
+  const { stdout } = await execa.command('npm --version')
+  return stdout
+}
+
+const getLatestNpm = async function () {
+  const {
+    body: { version: npmVersion },
+  } = await got(NPM_LATEST_URL, { responseType: 'json' })
+  return npmVersion
+}
+
+const NPM_LATEST_URL = 'https://registry.npmjs.com/npm/latest'
+
+const checkNpmVersion = checkVersion.bind(undefined, 'npm', {
+  current: getCurrentNpm,
+  latest: getLatestNpm,
+})
 
 export const checkVersions = async () => {
   await Promise.all([checkNodeVersion(), checkNpmVersion()])
