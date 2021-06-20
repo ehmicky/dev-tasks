cbab9fb2f2f9eb6d4afbf28497f5d29b8e2ea432
ehmicky <ehmicky@gmail.com>
2021-06-20 23:29:42 +0200

    Fix linting

--- src/files.js
+++ src/files.js
@@ -5,7 +5,7 @@ const SRC_DIRS = ['src', 'benchmarks', 'docs', 'examples', 'gulp']
 const TEST_DIRS = ['test']
 const DIRS = [...SRC_DIRS, ...TEST_DIRS]
 // `test` snapshots should not be linted/prettified
-export const JAVASCRIPT = `{,{${DIRS.join(',')}}/{,**/}}*.js`
+export const JAVASCRIPT = `{,{${DIRS.join(',')}}/{,**/}}*.{js,cjs,mjs}`
 export const MARKDOWN = `{,{${SRC_DIRS.join(',')}}/{,**/}}*.md`
 export const JSON_YAML = [
   `{,{${SRC_DIRS.join(',')}}/{,**/}}*.{json,yml}`,
