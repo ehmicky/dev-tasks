1a4ae3e8f8b1d3db0e1b0d74f0fd86fbda3bfdd9
ehmicky <ehmicky@gmail.com>
2021-06-21 17:06:22 +0200

    Fix linting

--- test/main.js
+++ test/main.js
@@ -1,6 +1,5 @@
-// eslint-disable-next-line node/no-missing-import, import/no-unresolved
 import { build } from '@ehmicky/dev-tasks'
-// eslint-disable-next-line node/no-missing-import, import/no-unresolved, import/no-unassigned-import
+// eslint-disable-next-line import/no-unassigned-import
 import '@ehmicky/dev-tasks/register.js'
 import test from 'ava'
 
