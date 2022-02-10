43b637ea0221c9c2bbd152d9c7e911b9ebd87d33
ehmicky <ehmicky@gmail.com>
2022-02-10 18:44:46 +0100

    Revert "Use Ava v4"
    This reverts commit 9c2b8293903bb6160cea17e31be426bf78f5be91.

--- ava.config.cjs
+++ ava.config.cjs
@@ -1,21 +1,19 @@
 // Ava requires configuration file to be a the repository's top-level
-import { fileURLToPath } from 'url'
 
 const SRC = 'src'
 const BUILD_TEST = 'build/test'
 
-const LOG_PROCESS_ERRORS = fileURLToPath(
-  new URL('build/src/tasks/unit/log_process_errors.js', import.meta.url),
-)
+const LOG_PROCESS_ERRORS = `${__dirname}/build/src/tasks/unit/log_process_errors.js`
 
-// eslint-disable-next-line import/no-anonymous-default-export, import/no-default-export
-export default {
+// Ava configuration file does not fully support ES modules yet
+module.exports = {
   // We watch only for `*.js` files, otherwise `*.js.map` gets watched and it
   // creates issues.
   files: [`${BUILD_TEST}/**/*.js`, `!${BUILD_TEST}/helpers`],
   // Otherwise, if build watch is run as well, modifying source files trigger
   // tests twice
   ignoredByWatcher: [SRC],
+  verbose: true,
   // Use `log-process-errors`
   require: [LOG_PROCESS_ERRORS],
   timeout: `3600s`,
--- src/tasks/cover/c8.js
+++ src/tasks/cover/c8.js
@@ -4,6 +4,6 @@ import { shouldCover } from './utils.js'
 // Locally, one must directly call `c8 ava`
 export const getC8 = function () {
   return shouldCover()
-    ? `c8 --exclude=build/test --exclude=ava.config.js --reporter=lcov --reporter=text --reporter=html --reporter=json `
+    ? `c8 --exclude=build/test --exclude=ava.config.cjs --reporter=lcov --reporter=text --reporter=html --reporter=json `
     : ''
 }
