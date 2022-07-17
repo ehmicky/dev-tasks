3f81b3b08524fbb29d707562b2017489b48d49f9
ehmicky <ehmicky@gmail.com>
2022-07-17 15:41:41 +0200

    Improve performance

--- src/tasks/test.js
+++ src/tasks/test.js
@@ -5,7 +5,8 @@ import { check } from './check/main.js'
 import { type } from './type.js'
 import { unit } from './unit/main.js'
 
-export const testTask = gulp.series(check, build, type, unit)
+const postBuildTest = gulp.parallel(type, unit)
+export const testTask = gulp.series(check, build, postBuildTest)
 
 // eslint-disable-next-line fp/no-mutation
 testTask.description = 'Build, lint and test source files'
