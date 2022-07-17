81f67f055e978075e67cec8a450b7b5253ce1171
ehmicky <ehmicky@gmail.com>
2022-07-17 15:41:29 +0200

    Fix gulp type

--- src/tasks/type.js
+++ src/tasks/type.js
@@ -1,6 +1,19 @@
-import { task } from 'gulp-execa'
+import { cwd } from 'process'
 
-export const type = task('tsd')
+import { exec } from 'gulp-execa'
+import { pathExists } from 'path-exists'
+
+export const type = async function () {
+  if (!(await pathExists('tsconfig.json')) || isSelf()) {
+    return
+  }
+
+  await exec('tsd')
+}
+
+const isSelf = function () {
+  return cwd().includes('dev-tasks')
+}
 
 // eslint-disable-next-line fp/no-mutation
 type.description = 'Run TypeScript type tests'
