ca2b35cf02fe22d085fcdac0564d0de29e6e97a7
ehmicky <ehmicky@gmail.com>
2022-11-06 10:03:43 +0100

    Try to improve Babel config

--- src/tasks/build/.babelrc.js
+++ src/tasks/build/.babelrc.js
@@ -3,12 +3,14 @@ import { readFileSync } from 'fs'
 // TODO: replace with JSON imports once supported
 const { dependencies = {} } = JSON.parse(readFileSync('./package.json'))
 
+const targets = readFileSync(new URL('browserslist', import.meta.url), 'utf8')
+
 export default {
   presets: [
     [
       '@babel/preset-env',
       {
-        targets: { node: '14.18.0' },
+        targets,
         modules: false,
         // Using `core-js` as a dependency is optional
         ...(dependencies['core-js'] && { useBuiltIns: 'usage', corejs: '3' }),
