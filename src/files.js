f37cd5bfee3ecf7f27cbed5014c182975ea90b52
ehmicky <ehmicky@gmail.com>
2022-11-27 17:54:03 +0100

    Refactoring

--- src/files.js
+++ src/files.js
@@ -1,15 +1,21 @@
-const SOURCES_DIRS = ['src', 'benchmark', 'docs', 'examples', 'gulp']
-export const BUILD_SOURCES = ['src', 'benchmark']
-export const BUILD = 'build'
+// Generated files that should be considered like source files
 export const GENERATED_SOURCES_DIR = 'src/snapshots'
+// Directories containing "sources" in the wide sense of the term
+const SOURCES_DIRS = ['src', 'benchmark', 'docs', 'examples', 'gulp']
+// Any file in the top-level directory or inside source-like directories
+const ANY_SOURCE_FILE = `{,{${SOURCES_DIRS.join(',')}}/{,**/}}*`
+// Source directories to build
+export const BUILD_SOURCES = ['src', 'benchmark']
+// Build directory
+export const BUILD = 'build'
 
-export const JAVASCRIPT = `{,{${SOURCES_DIRS.join(',')}}/{,**/}}*.{js,cjs,mjs}`
-export const MARKDOWN = `{,{${SOURCES_DIRS.join(',')}}/{,**/}}*.md`
-export const JSON_YAML = [
-  `{,{${SOURCES_DIRS.join(',')}}/{,**/}}*.{json,yml}`,
-  '.*.{json,yml}',
-]
-export const TYPES = ['src/**/*.ts', 'tsconfig.json']
-export const TYPE_TESTS = 'src/**/*.test-d.ts'
+// Source files, per file type or category
+export const JAVASCRIPT = `${ANY_SOURCE_FILE}.{js,cjs,mjs}`
+export const MARKDOWN = `${ANY_SOURCE_FILE}.md`
+export const JSON_YAML = [`${ANY_SOURCE_FILE}.{json,yml}`, '.*.{json,yml}']
+export const TYPESCRIPT = 'src/**/*.ts'
+export const TYPESCRIPT_TESTS = 'src/**/*.test-d.ts'
+export const TYPESCRIPT_CONFIG = 'tsconfig.json'
+// Those source files should not be linted nor prettified
 export const IGNORED_SOURCES = [`src/snapshots/**`, `src/fixtures/invalid*/**`]
 export const DEPENDENCIES = ['package-lock.json']
