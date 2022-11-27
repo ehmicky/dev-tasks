ab863f742604628a8c7903388dcb1daf749f9e31
ehmicky <ehmicky@gmail.com>
2022-11-27 17:58:07 +0100

    Improve jscpd

--- src/tasks/check/jscpd.js
+++ src/tasks/check/jscpd.js
@@ -1,6 +1,8 @@
 import { fileURLToPath } from 'node:url'
 
-import { task } from 'gulp-execa'
+import { exec } from 'gulp-execa'
+import pFilter from 'p-filter'
+import { pathExists } from 'path-exists'
 
 // import { JAVASCRIPT } from '../../files.js'
 
@@ -10,7 +12,20 @@ const JSCPD_CONFIG = fileURLToPath(new URL('.jscpd.json', import.meta.url))
 // is cross-files.
 // We cannot expand globbing patterns ourselves because it can hit the CLI
 // max length.
-export const jscpd = task(
-  `jscpd --config=${JSCPD_CONFIG} --pattern=**/*.ts --pattern=**/*.js --ignore=**/*.test-d.ts src benchmark docs examples gulp`,
-  { echo: false },
-)
+export const jscpd = async function () {
+  const sourceDirs = await pFilter(
+    ['src', 'benchmark', 'docs', 'examples', 'gulp'],
+    pathExists,
+  )
+
+  if (sourceDirs.length === 0) {
+    return
+  }
+
+  await exec(
+    `jscpd --config=${JSCPD_CONFIG} --pattern=**/*.ts --pattern=**/*.js --ignore=**/*.test-d.ts ${sourceDirs.join(
+      ' ',
+    )}`,
+    { echo: false },
+  )
+}
