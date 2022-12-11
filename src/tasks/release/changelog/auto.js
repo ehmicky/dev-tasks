891d1a03aec4592172542683c8bf27ad730d9a9e
ehmicky <ehmicky@gmail.com>
2022-12-11 23:49:28 +0100

    Improve release task

--- src/tasks/release/changelog/auto.js
+++ src/tasks/release/changelog/auto.js
@@ -1,10 +1,11 @@
 import { execa } from 'execa'
+import { valid } from 'semver'
 
 // If `CHANGELOG.md` is empty, use `git log` as a fallback
 export const printAutoChangelog = async function () {
   const { stdout: lastTag } = await execa('git', ['describe', '--abbrev=0'])
 
-  if (!lastTag.startsWith('v')) {
+  if (valid(lastTag) === null) {
     throw new TypeError('Could not find last git tag.')
   }
 
