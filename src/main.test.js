e4d02b104c1c899d1ee1ca58aeaf40b00aefac04
ehmicky <ehmicky@gmail.com>
2023-05-07 18:09:47 +0100

    Fix linting

--- src/main.test.js
+++ src/main.test.js
@@ -3,6 +3,7 @@ import { build } from '@ehmicky/dev-tasks'
 
 import '@ehmicky/dev-tasks/register.js'
 
+// eslint-disable-next-line import/order
 import test from 'ava'
 
 test('Dummy test', (t) => {
